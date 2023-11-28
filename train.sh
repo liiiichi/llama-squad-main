896a334de40ccf1fbb08838d1bcac4c5c26dee6e
liiiichi <avrilxu008@gmail.com>
2023-11-28 17:05:35 +0800

    add CUDA_VISIBLE_DEVICES in train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-python train_llama_squad.py \
---model_name togethercomputer/Llama-2-7B-32K-Instruct \
---dataset_name data/squad_v2 \
---bf16 \
---max_seq_length 32768 \
---per_device_train_batch_size 2 \
---gradient_accumulation_steps 4 \
---max_steps 10000 \
---merge_and_push \
---save_steps 1000 \
---learning_rate=2e-7
+CUDA_VISIBLE_DEVICES=0,1 python train_llama_squad.py \
+    --model_name togethercomputer/Llama-2-7B-32K-Instruct \
+    --dataset_name data/squad_v2 \
+    --bf16 \
+    --max_seq_length 32768 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --max_steps 10000 \
+    --merge_and_push \
+    --save_steps 1000 \
+    --learning_rate=2e-7
